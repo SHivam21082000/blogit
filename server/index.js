eb0ac183403483cbf75ff7b5b19c12a999dce11c
Shivam Shukla <75559756+SHivam21082000@users.noreply.github.com>
2021-10-05 16:28:13 +0530

    rest api done

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const multer = require('multer');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
@@ -6,8 +7,11 @@ const authRouter = require('./routes/auth')
 const userRouter = require('./routes/users')
 const postRouter = require('./routes/posts')
 const categoryRouter = require('./routes/categories');
+const path = require("path");
+
 dotenv.config()
 app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const DB = process.env.MONGO_URL;
 mongoose.connect(DB).then(()=>{
@@ -18,6 +22,20 @@ mongoose.connect(DB).then(()=>{
 app.get('/',(req,res)=>{
     res.send('Home page')
 })
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, "hello.png");
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
 
 app.use("/auth", authRouter)
 app.use('/user', userRouter)
